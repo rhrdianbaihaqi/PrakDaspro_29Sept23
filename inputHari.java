e07f081d3f387547c9cd74d67792773b9bf48019
rhrdianbaihaqi <144593465+rhrdianbaihaqi@users.noreply.github.com>
2023-09-29 22:30:35 +0700

    Add files via upload

---
+++ inputHari.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class inputHari {
+        public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan kode Hari: ");
+            // input hari disini
+            int kodeHari = input.nextInt();
+            int Hari = kodeHari;
+    
+            // Check if hari
+            if (Hari == 1) {
+                System.out.println("Senin");
+            } else if (Hari == 2) {
+                System.out.println("Selasa");
+            } else if (Hari == 3) {
+                System.out.println("Rabu");
+            } else if (Hari == 4) {
+                System.out.println("Kamis");
+            } else if (Hari == 5) {
+                System.out.println("Jumat");
+            } else if (Hari == 6) {
+                System.out.println("Sabtu");
+            }  else if (Hari == 7) {
+                System.out.println("Minggu");
+            }  else if (Hari == 8) {
+                System.out.println("Hari raya idhul Fitri");
+            }  else if (Hari == 9) {
+                System.out.println("Hari raya idhul Adha");
+            }  else if (Hari == 10) {
+                System.out.println("Hari kenaikan isa al masih");
+            }  else if (Hari == 11) {
+                System.out.println("Hari buruh nasional");
+            }  else if (Hari == 12) {
+                System.out.println("Hari Nyepi");
+            }  else if (Hari == 13) {
+                System.out.println("Hari Natal");
+            }  else if (Hari == 14) {
+                System.out.println("Hari Libur");
+            }  else if (Hari == 15) {
+                System.out.println("Tahun Baru");
+            } 
+            // Check else data
+            else {
+                System.out.println ("ANDA SALAH MEMASUKAN DATA !!! PILIH ANGKA 1-15");
+            }
+        }
+    }
+
---
+++ ngantukTidur.java
@@ -0,0 +1,13 @@
+class ngantukTidur {
+    public static void main (String[] args) {
+        // create a string variable
+        String saya = "ngantuk";
+
+        // if statement
+        if (saya== "ngantuk") {
+        System.out.println ("saya akan pergi tidur");
+        } else {
+        System.out.println("saya akan pergi ke kampus");
+        }
+    }
+}
